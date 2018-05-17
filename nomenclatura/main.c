7bb520150a0459cbede71fcdff3bce6ab4c16a88
EzequielSegota <ezequielsegota@hotmail.com>
2018-05-17 10:25:13 -0300

    Punteros enteros

--- nomenclatura/main.c
+++ nomenclatura/main.c
@@ -6,15 +6,22 @@ int main()
     //punteros
     int *punteroNumero;
     int numero;
+    int numeroDos;
+    numeroDos=&numero;
     numero=66;
-    punteroNumero=&numero;
+    punteroNumero=&numero;//(*)
+    printf("\n a- %d",numeroDos);//direccion
     //*punteroNumero=&numero;
     //*punteroNumero=55;
+    numeroDos=*punteroNumero;
+    printf(" b- %d\n",numeroDos);
+
     printf("%d\n",numero);//valor de numero
-    printf("%d\n",&numero);//le pasamos donde empieza el elemento
-    printf("%d\n",punteroNumero);
-    printf("%d",&punteroNumero);//direccion donde esta guardado el puntero
-    //printf("%d",*punteroNumero);
+    printf("%d\n",numeroDos);
+    printf("%d\n",&numero);//le pasamos donde empieza el elemento(*) sino se tilda
+    printf("%p\n",punteroNumero);//direccion donde empieza el puntero
+    printf("%p\n",&punteroNumero);//direccion donde esta guardado el puntero
+    printf("%d",*punteroNumero);//muestra el contenido del puntero, puede dar error si inicialo el puntero y no le doy ningun valor o tomar un null
 
     return 0;
 }
