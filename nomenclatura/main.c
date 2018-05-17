65dd95229dd6148ee5059e62f27255fabbafb7ae
EzequielSegota <ezequielsegota@hotmail.com>
2018-05-17 10:05:44 -0300

    Punteros parte 1

--- nomenclatura/main.c
+++ nomenclatura/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //punteros
+    int *punteroNumero;
+    int numero;
+    numero=66;
+    punteroNumero=&numero;
+    //*punteroNumero=&numero;
+    //*punteroNumero=55;
+    printf("%d\n",numero);//valor de numero
+    printf("%d\n",&numero);//le pasamos donde empieza el elemento
+    printf("%d\n",punteroNumero);
+    printf("%d",&punteroNumero);//direccion donde esta guardado el puntero
+    //printf("%d",*punteroNumero);
+
     return 0;
 }
